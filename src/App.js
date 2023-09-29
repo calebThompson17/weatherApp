38385e1190072937562bf54c977aa27e42e73122
calebThompson17 <caleb.thompson.17@cnu.edu>
2023-09-29 15:38:36 -0400

    Getting initial weather api call response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const apiKey = 'beb1478e094bd8bf3d03026212ece3bf';
+
+  useEffect(() => {
+    getWeather(38.889363, -77.036045);
+  }, []);
+
+  useEffect(() => {
+    console.log('data:', data);
+  }, [data]);
+
+  const getWeather = (lat, lon) => {
+    const weatherApiRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(weatherApiRequest)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +29,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
