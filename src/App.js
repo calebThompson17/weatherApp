e49711bfed7438f908767ab3b7e5455737f596ad
calebThompson17 <caleb.thompson.17@cnu.edu>
2023-09-29 16:03:50 -0400

    Now displaying temperature and converting it to Celsius

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const [location, setLocation] = useState(null);
   const [data, setData] = useState(null);
+  const [temperature, setTemperature] = useState(null);
   const apiKey = 'beb1478e094bd8bf3d03026212ece3bf';
 
   useEffect(() => {
@@ -21,6 +22,18 @@ function App() {
 
   useEffect(() => {
     console.log('data:', data);
+    if (data) {
+      const kelvin = {
+        temp: data.main.temp,
+        feels_like: data.main.feels_like,
+        temp_min: data.main.temp_min,
+        temp_max: data.main.temp_max,
+      };
+
+      const newTemperature = kelvin;
+      console.log('newTemperature:', newTemperature);
+      setTemperature(newTemperature);
+    }
   }, [data]);
 
   const getLocation = () => {
@@ -37,12 +50,21 @@ function App() {
       .catch(error => console.error(error));
   }
 
+  const kelvinToCelsius = (kelvin) => {
+    return kelvin - 273.15;
+  }
+
+  const kelvinToFahrenheit = (kelvin) => {
+    return ((kelvin - 273.15) * 9/5) + 32;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Current Weather:</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Temperature: {kelvinToCelsius(temperature?.temp).toFixed(2)} °C
         </p>
       </header>
     </div>
